42fa54469b2057c1cfff1723030365381409d755
Artur Vorobiov <arturv@wix.com>
2022-05-01 16:43:29 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Sidebar} from './Sidebar';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Twitter Clone!</h1>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
