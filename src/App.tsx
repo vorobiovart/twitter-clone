3249d8251d7cfec398ae3744ea384afa8b2ec564
Artur Vorobiov <arturv@wix.com>
2022-05-06 12:45:33 +0200

    starter app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import {Feed} from './Feed';
-import {Layout} from './Layout';
-import {Notifications} from './Notifications';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Feed />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/notifications" element={<Notifications />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h2>Starter App</h2>
+    </div>
   );
 }
 
