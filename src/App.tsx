1f2d7426b02e86e250ed378b1b8eccc0823699f3
Artur Vorobiov <arturv@wix.com>
2022-05-12 09:48:11 +0200

    rating component

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,10 @@
 import React from 'react';
+import {RatingComponent} from './RatingComponent';
 
 function App() {
-  // Find the largest subset of unique numbers in an array of numbers.
-  const findLargestSubset = (data: number[]) => {
-    return Array.from(new Set(data));
-  };
-
-  console.log(findLargestSubset([1, 1, 2, 3, 4, 4, 5, 5, 6, 6, 2, 1, 5, 8, 9, 0, 12, 34, 56, 56]));
-
-  // Roman to Integer
-  const romanToInt = (s) => {
-    const convertor = {
-      I: 1,
-      V: 5,
-      X: 10,
-      L: 50,
-      C: 100,
-      D: 500,
-      M: 1000,
-    };
-
-    let result = 0;
-
-    for (let i = 0; i < s.length; i++) {
-      if (convertor[s[i]] < convertor[s[i + 1]]) {
-        result += convertor[s[i + 1]] - convertor[s[i]];
-        i = i + 1;
-        continue;
-      }
-      result += convertor[s[i]];
-    }
-
-    return result;
-  };
-
-  console.log(romanToInt('III'));
-
   return (
     <div>
-      <h2>asd</h2>
+      <RatingComponent starsNumber={5} />
     </div>
   );
 }
---
+++ src/algos.ts
@@ -0,0 +1,34 @@
+// Find the largest subset of unique numbers in an array of numbers.
+export const findLargestSubset = (data: number[]) => {
+  return Array.from(new Set(data));
+};
+
+// console.log(findLargestSubset([1, 1, 2, 3, 4, 4, 5, 5, 6, 6, 2, 1, 5, 8, 9, 0, 12, 34, 56, 56]));
+
+// Roman to Integer
+export const romanToInt = (s) => {
+  const convertor = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (convertor[s[i]] < convertor[s[i + 1]]) {
+      result += convertor[s[i + 1]] - convertor[s[i]];
+      i = i + 1;
+      continue;
+    }
+    result += convertor[s[i]];
+  }
+
+  return result;
+};
+
+// console.log(romanToInt('III'));
