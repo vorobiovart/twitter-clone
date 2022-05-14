ba01e310a798b53e1f8f9a5cfbc0bac35335f5cd
Artur Vorobiov <arturv@wix.com>
2022-05-14 17:01:52 +0200

    rating

--- src/RatingComponent/RaitingComponent.tsx
+++ src/RatingComponent/RaitingComponent.tsx
@@ -7,23 +7,21 @@ interface RatingComponentProps {
 }
 
 export const RatingComponent: React.FC<RatingComponentProps> = (props) => {
-  const [hover, setHover] = useState(undefined);
   const [rating, setRating] = useState(undefined);
+  const [hover, setHover] = useState(undefined);
 
   return (
     <div className={s.root}>
-      {[...Array(props.starsNumber)].map((_star, index) => {
-        return (
-          <div
-            className={s.star}
-            onMouseEnter={() => setHover(index)}
-            onMouseLeave={() => setHover(undefined)}
-            onClick={() => setRating(index)}
-          >
-            {index <= (hover || rating) ? <span>&#9733;</span> : <span>&#9734;</span>}
-          </div>
-        );
-      })}
+      {[...Array(props.starsNumber)].map((_star, index) => (
+        <div
+          className={s.star}
+          onMouseEnter={() => setHover(index)}
+          onMouseLeave={() => setHover(undefined)}
+          onClick={() => setRating(index)}
+        >
+          {index <= (hover || rating) ? <span>&#9733;</span> : <span>&#9734;</span>}
+        </div>
+      ))}
     </div>
   );
 };
