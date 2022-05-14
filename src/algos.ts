e243d8d22ad64a7c87c8659d526237931d6acb7b
Artur Vorobiov <arturv@wix.com>
2022-05-14 16:15:14 +0200

    algos

--- src/algos.ts
+++ src/algos.ts
@@ -31,4 +31,60 @@ export const romanToInt = (s) => {
   return result;
 };
 
-// console.log(romanToInt('III'));
+console.log(romanToInt('III'));
+
+const sortCollection = (collection) => {
+  return collection.sort((a, b) => {
+    if (a.name > b.name) return 1;
+    if (a.name < b.name) return -1;
+
+    if (a.age > b.age) return 1;
+    if (a.age < b.age) return -1;
+
+    return 0;
+  });
+};
+
+const collection = [
+  {name: 'Sarah', age: 19},
+  {name: 'Artur', age: 30},
+  {name: 'Aba', age: 25},
+  {name: 'Brat', age: 29},
+  {name: 'Artur', age: 31},
+  {name: 'Tom', age: 20},
+  {name: 'Brat', age: 29},
+  {name: 'Brat', age: 28},
+];
+console.log(sortCollection(collection));
+
+// Write a function that takes input of arrays and loops over them to calculate return values based on some criteria.
+// For example, take this array [['oranges', 1], ['apples', 2], ['grapes', 7]].
+// Based on this array return total costs if second array property is a quantity and product is first array item.
+// If say Oranges cost $5, and user buys the quantity of 2 or more, apply a 20% discount etc..
+// Return total cost from array by adding them all up.
+
+const someWeirdFn = (products, criteria) => {
+  const {productName, amountToBuy, price, discount, discountStartsAfterItemsPurchased} = criteria;
+
+  const product = products.find(([name]) => name === productName);
+  if (!product) return 'No such product';
+
+  const [_name, amountAvailable] = product;
+  if (amountAvailable < amountToBuy) return 'Not enough items';
+
+  if (amountToBuy >= discountStartsAfterItemsPurchased) {
+    return amountToBuy * price * (1 - discount);
+  }
+  return amountToBuy * price;
+};
+
+console.log(
+  someWeirdFn(
+    [
+      ['oranges', 1],
+      ['apples', 2],
+      ['grapes', 7],
+    ],
+    {productName: 'grapes', amountToBuy: 3, price: 10, discount: 0.2, discountStartsAfterItemsPurchased: 2},
+  ),
+);
